ebb7fdf39c6f61e2ac8b09e1bd289def7dc5886d
Chad Mueller <chad@websitesbeyond.com>
2024-06-02 23:50:53 -0700

    add initial class skeleton

---
+++ models/drill.php
@@ -0,0 +1,4 @@
+<?php
+class Drill extends Equipment {
+    public $drillSpeed;
+}
---
+++ models/materials.php
@@ -0,0 +1,15 @@
+<?php
+
+class Materials {
+    public $id;
+    public $type;
+    public $quantity;
+
+    public function updateStock() {
+        // under construction
+    }
+
+    public function order() {
+        // under construction
+    }
+}
